b130c97be5e50949e2cb4acfc74d5d7fede6f571
Vamsi Balimidi <vamsi.balimidi@gmail.com>
2025-03-18 22:47:55 +0000

    Change so that the fold-expr happens only for .c files

--- custom/keymaps/options.lua
+++ custom/keymaps/options.lua
@@ -1,5 +1,12 @@
 print("Custom Options")
 -- Fold options
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "c",
+  callback = function()
+    vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  end,
+})
+
 vim.opt.foldlevel = 5
